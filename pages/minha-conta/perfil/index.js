6acae370bc8b8a8f86fd57ae558861e63f015a27
Andre Santin <andre.santin@mjv.com.br>
2021-07-23 10:25:48 -0300

    user persistindo o estado

--- pages/minha-conta/perfil/index.js
+++ pages/minha-conta/perfil/index.js
@@ -6,13 +6,15 @@ import { useSelector } from 'react-redux'
 const MeuPerfil = () => {
 
     const user = useSelector(state => state.user);
-
-    const perfil = user.user.user
+    const perfil = user.user.user;
+    
     return(
     <Layout>
     <h5>MEU PERFIL</h5>    
-    <h2>{perfil.user_metadata.full_name}</h2>
     <img src={perfil.user_metadata.avatar_url} />
+    <h2>{perfil.user_metadata.full_name}</h2>
+    <h2>{perfil.email}</h2>
+
     </Layout>
     )
 }
